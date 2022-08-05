e09ec8be202786344698cbc19a555b0c9adbcd3c
Carl Jones III <carl.jones@perficient.com>
2022-08-04 21:28:42 -0400

    completed selenium setup, adding selenium functions test components

--- src/test/java/StepDefinitions/NavSteps.java
+++ src/test/java/StepDefinitions/NavSteps.java
@@ -7,15 +7,31 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
+import java.util.concurrent.TimeUnit;
+
 public class NavSteps {
 
 //    @Given("user is on home page")
 //    public void verifyHomepageNavigation(){
 //        System.out.println("Time to test BMW.com!");
 //    }
-    @Given("user is on home page")
+    WebDriver driver;
+
+    @Given("user has started a web browser")
+    public void user_generates_browser(){
+        WebDriverManager.chromedriver().setup();
+
+        driver = new ChromeDriver();
+
+        driver.manage().timeouts().implicitlyWait(40, TimeUnit.SECONDS);
+        driver.manage().timeouts().pageLoadTimeout(40, TimeUnit.SECONDS);
+
+        driver.manage().window().maximize();
+    }
+    @And("user is on home page")
     public void user_navigated_to_home_page(){
 
+        driver.navigate().to("https://www.bmw.com/en/index.html");
         System.out.println("Step Detail - user is navigated to homepage");
 
     }
--- target/test-classes/Features/home.feature
+++ target/test-classes/Features/home.feature
@@ -8,7 +8,8 @@ Feature: feature to test loading of the Homepage
   @regressiontest
   Scenario: Check the ui elements of the homepage Navigation
 
-    Given user is on home page
+    Given user has started a web browser
+    And user is on home page
     When home page loads
     And user clicks on tab home
     Then user is redirected to the current page of the web application
